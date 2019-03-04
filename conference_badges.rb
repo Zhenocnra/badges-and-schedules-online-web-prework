fce98c8e17c1ff60ac0f6f8a0c21056b5ca3a951
Zoncharne Lee <zoncharne@gmail.com>
2019-03-04 05:09:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,13 @@ def badge_maker(name)
 "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+
   
