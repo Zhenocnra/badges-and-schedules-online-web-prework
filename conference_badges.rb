962199bf46d0ca7b8f5b7a48d30890acb20e776b
Zoncharne Lee <zoncharne@gmail.com>
2019-03-04 05:16:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,12 +5,12 @@ end
 def batch_badge_creator(attendees)
   array = []
   attendees.each do |name|
-    array.push("Hello, my name is #{name}.")
+    array.push(badge_maker(name))
   end
   return array
 end
 
-def assign_rooms(array)
+def assign_rooms(attendees))
   i = 1
   array = []
   attendees.each do |name|
@@ -21,4 +21,11 @@ def assign_rooms(array)
 end
 
 def printer(attendees)
+  batch_badge_creator(attendees).each do |result|
+    puts result
+  end
+  assign_rooms(attendees).each do |result|
+    puts result
+  end
+end
   
